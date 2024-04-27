e183d7f7a3e3a6106b788ecf7f793feac03c38b5
OlenaShevelenko31 <shevelenko.work@gmail.com>
2024-04-26 21:15:29 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,144 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>> Motivation element with qoutes <<<<<<<<<<<<<<<<<<<<<<<<<<
+const button = document.getElementById ("btn");
+const par = document.getElementById("quote");
+
+const quotes =[
+    "A bad day doesn’t equal a bad life.    ",
+    "There are billions of cells in your body and all they care about is you.",
+    "Just because you take breaks doesn’t mean you’re broken.– Curtis Tyrone Jones",
+    "Your feelings are always valid, no matter what they are.",
+    "Your past does not define you.",
+    "You do not need the approval of others to feel happy.",
+    "It’s a new day: kick ass, walk tall, and sparkle!",
+    "“Beauty begins the moment you decide to be yourself.” -Coco Chanel",
+    "“Believe you can and you’re halfway there.” -Theodore Roosevelt"
+]
+button.addEventListener('click', ()=>{
+    let randomQuate = quotes [Math.floor(Math.random()* quotes.length)];
+    par.style.display = "block";
+    par.textContent = randomQuate;
+})
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>> TIMER starts <<<<<<<<<<<<<<<<<<<<<<<<<<
+
+let timerID;
+const startButton = document.querySelector(".start");
+const stopButton = document.querySelector(".stop");
+
+startButton.addEventListener("click", function () {
+    timer();
+});
+
+stopButton.addEventListener("click", function () {
+    stopTimer();
+});
+
+const time = 30;
+let totalTime = time * 60;
+
+function timer() {
+    startButton.setAttribute("disabled", true);
+
+    function countdown() {
+        const countdownElement = document.querySelector("#countdown");
+        countdownElement.style.display = "block";
+        let min = Math.floor(totalTime / 60);
+        let sec = totalTime % 60;
+
+        totalTime--;
+
+        if (sec < 10) {
+            sec = "0" + sec;
+        }
+
+        countdownElement.textContent = `${min}:${sec}`;
+
+        if (totalTime < 0) {
+            stopTimer();
+            totalTime = 0;
+            // window.alert()
+            Swal.fire({
+                title: 'Timer is over!',
+                imageUrl: 'https://images.unsplash.com/photo-1501743411739-de52ea0ce6a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+                imageWidth: 600,
+                imageHeight: 400,
+                imageAlt: 'Victory!',
+              })
+    }
+    }
+
+    timerID = setInterval(countdown, 1000);
+
+}
+
+function stopTimer() {
+    clearInterval(timerID);
+    startButton.removeAttribute("disabled");
+}
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>> TIMER ends <<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>> To do list input element <<<<<<<<<<<<<<<<<<<<<<<<<<
+const input = document.querySelector ('.input');
+const btn = document.querySelector ('.btn');
+const list = document.querySelector ('.list');
+
+const selectedDay = document.getElementById("day");
+const mondayList = document.querySelector(".monday.list");
+const tuesdayList = document.querySelector(".tuesday.list");
+const wednesdayList = document.querySelector(".wednesday.list");
+const thursdayList = document.querySelector(".thursday.list");
+const fridayList = document.querySelector(".friday.list");
+const saturdayList = document.querySelector(".saturday.list");
+const sundayList = document.querySelector(".sunday.list");
+
+
+btn.addEventListener ('click', function(){
+    const item = document.createElement ('li');
+    const clone = template.content.cloneNode(true);
+
+    item.innerText = input.value;
+    if (input.value.length === 0) {
+        return false;
+    }
+    item.classList.add ('styleList');
+    switch(selectedDay.value) {
+        case 'monday':
+            mondayList.appendChild(item);
+            break;
+        case 'tuesday':
+            tuesdayList.appendChild(item);
+            break;
+        case 'wednesday':
+            wednesdayList.appendChild(item);
+            break;
+        case 'thursday':
+            thursdayList.appendChild(item);
+            break;
+        case 'friday':
+            fridayList.appendChild(item);
+            break;
+        case 'saturday':
+            saturdayList.appendChild(item);
+            break;
+        case 'sunday':
+            sundayList.appendChild(item);
+            break;
+        default:
+            list.appendChild(item);s
+            break;
+    }
+
+    item.addEventListener ('click', function(){
+        item.classList.add ('toDoCompated');
+    })
+
+    item.addEventListener('dblclick', function(){
+        const parentList = item.parentElement; 
+        parentList.removeChild(item);      
+
+        window.alert('To-Do item succescfully finished! Yay ');
+
+    })
+})
+
+
